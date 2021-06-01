876d63a331a604da182bfbf525ede7657e6e240a
RamsesVCode <yaphets291000@gmail.com>
2021-06-01 17:57:21 -0400

    Borrar entrada listo

--- entrada.php
+++ entrada.php
@@ -3,6 +3,7 @@
 <?php if(!isset($_SESSION)) session_start();?> 
 <div id="principal">
         <?php $entrada = getEntrada($db,$_GET['id']);?>
+        <?php //var_dump($entrada);die();?>
         <?php if(!empty($entrada)): ?>    
         <h1><?=$entrada['TITULO'];?></h1>
         <article class="entrada">
@@ -16,6 +17,12 @@
                 <?=$entrada['DESCRIPCION'];?>
             </p>
         </article>
+        <?php if(isset($_SESSION['usuario']) && $entrada['USUARIO_ID']==$_SESSION['usuario']['ID']): ?>
+        <div class="botones">
+            <a class="boton boton-verde" href="editar-entrada.php?id=<?=$entrada['ID'];?>">Editar</a>
+            <a class="boton boton-rojo" href="borrar-entrada.php?id=<?=$entrada['ID'];?>">Eliminar</a>
+        </div>
+        <?php endif;?>
         <?php endif; ?>
     </div><!--Fin principal-->
         <div class="clearfix"></div>
